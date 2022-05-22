184b60173a16b838272403f7169ff3d22d7d0603
Nicolas C <nicolas.corminboeuf@he-arc.ch>
2022-05-22 21:09:02 +0200

    La feature ImagePicker place l'image au bon endroit : TODO : persistance

--- lib/screens/taskpage.dart
+++ lib/screens/taskpage.dart
@@ -19,71 +19,91 @@ class _TaskpageState extends State<Taskpage> {
   Color myColor = Colors.white;
   File? image;
 
+  //Méthodes pour l'image picker
+  Future pickImage(ImageSource source) async {
+    try {
+      final image = await ImagePicker().pickImage(source: source);
+
+      if (image == null) return;
+
+      final imageTemp = File(image.path);
+      setState(() => this.image = imageTemp);
+    } on PlatformException catch (e) {
+      print('Impossible de recuprer l"image: $e');
+    }
+  }
 
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      backgroundColor: myColor,
-      body: SafeArea(
+        backgroundColor: myColor,
+        body: SafeArea(
           child: Container(
-        child: Stack(
-          children: [
-            Column(
-              crossAxisAlignment: CrossAxisAlignment.start,
-              children: [
-                Padding(
-                  padding: const EdgeInsets.symmetric(vertical: 24.0),
-                  child: Row(
-                    children: [
-                      GestureDetector(
-                        onTap: () {
-                          Navigator.pop(context);
-                        },
-                        child: const Padding(
-                          padding: EdgeInsets.all(24.0),
-                          child: Icon(
-                            Icons.arrow_back_rounded,
-                            color: Colors.blueGrey,
-                            size: 36.0,
+            child: Stack(
+                children: [
+              Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  image != null
+                      ? Image.file(
+                          image!,
+                          width: 600,
+                          height: 300,
+                        )
+                      : FlutterLogo(size: 160),
+                  Padding(
+                    padding: const EdgeInsets.symmetric(vertical: 24.0),
+                    child: Row(
+                      children: [
+                        GestureDetector(
+                          onTap: () {
+                            Navigator.pop(context);
+                          },
+                          child: const Padding(
+                            padding: EdgeInsets.all(24.0),
+                            child: Icon(
+                              Icons.arrow_back_rounded,
+                              color: Colors.blueGrey,
+                              size: 36.0,
+                            ),
                           ),
                         ),
-                      ),
-                      const Expanded(
-                          child: TextField(
-                        decoration: InputDecoration(
-                          hintText: "Entrer le titre de la tâche",
+                        const Expanded(
+                            child: TextField(
+                          decoration: InputDecoration(
+                            hintText: "Entrer le titre de la tâche",
+                            border: InputBorder.none,
+                          ),
+                          style: TextStyle(
+                            fontSize: 26.0,
+                            fontWeight: FontWeight.bold,
+                            color: Colors.blueGrey,
+                          ),
+                        ))
+                      ],
+                    ),
+                  ),
+                  const Padding(
+                    padding: EdgeInsets.only(
+                      bottom: 8.0,
+                    ),
+                    child: TextField(
+                      decoration: InputDecoration(
+                          hintText: "Entrer le description de la tâche",
                           border: InputBorder.none,
-                        ),
-                        style: TextStyle(
-                          fontSize: 26.0,
-                          fontWeight: FontWeight.bold,
-                          color: Colors.blueGrey,
-                        ),
-                      ))
-                    ],
+                          contentPadding: EdgeInsets.symmetric(
+                            horizontal: 24.0,
+                          )),
+                    ),
                   ),
-                ),
-                const Padding(
-                  padding: EdgeInsets.only(
-                    bottom: 8.0,
-                  ),
-                  child: TextField(
-                    decoration: InputDecoration(
-                        hintText: "Entrer le description de la tâche",
-                        border: InputBorder.none,
-                        contentPadding: EdgeInsets.symmetric(
-                          horizontal: 24.0,
-                        )),
-                  ),
-                ),
-                ToDoWidget("vide", true),
-                ToDoWidget("vide", false),
-              ],
-            ),
+                  ToDoWidget("vide", true),
+                  ToDoWidget("vide", false),
+                ],
+              ),
             ]),
           ),
-      ),
+        ),
         floatingActionButton: FloatingActionButton.extended(
             elevation: 4.0,
             icon: const Icon(Icons.add),
@@ -93,126 +113,88 @@ class _TaskpageState extends State<Taskpage> {
         floatingActionButtonLocation: FloatingActionButtonLocation.endDocked,
         bottomNavigationBar: BottomAppBar(
           color: Colors.white70,
-          child: Row(
-              mainAxisSize: MainAxisSize.max,
-              children: <Widget>[
-                IconButton(
-                  iconSize: 36.0,
-                  icon: Icon(
-                    Icons.color_lens_outlined,
-                  ),
-                  onPressed: () {
-                    showDialog(
-                        context: context,
-                        builder: (BuildContext context) {
-                          return AlertDialog(
-                            title: Text('Choisis une couleur'),
-                            content: SingleChildScrollView(
-                              child: ColorPicker(
-                                pickerColor: myColor, //default color
-                                onColorChanged: (Color color) {
-                                  //on color picked
-                                  setState(() {
-                                    myColor = color;
-                                  });
-                                },
-                              ),
-                            ),
-                            actions: <Widget>[
-                              ElevatedButton(
-                                child: const Text('Valider'),
-                                onPressed: () {
-                                  Navigator.of(context)
-                                      .pop(); //dismiss the color picker
-                                },
-                              ),
-                            ],
-                          );
-                        });
-                  },
-                ),
-                IconButton(
-                  icon: Icon(Icons.image),
-                  onPressed: () {
-                    showDialog(
-                        context: context,
-                        builder: (BuildContext context) {
-                          return AlertDialog(
-                            title: Text('Importer une image'),
-                            actions: <Widget>[
-                              MaterialButton(
-                                  child: Icon(Icons.image),
-                                  onPressed: () {
-                                    pickImageGallery();
-                                  }),
-                              MaterialButton(
-                                  child: Icon(Icons.camera_alt),
-                                  onPressed: () {
-                                    pickImageCamera();
-                                  }),
-                              MaterialButton(
-                                child: Icon(Icons.close),
-                                onPressed: () {
-                                  Navigator.of(context)
-                                      .pop(); //dismiss the color picker
-                                },
-                              ),
-                              SizedBox(
-                                width: 20,
-                              ),
-                              image != null
-                                  ? Image.file(image!)
-                                  : Text('No image selected')
-                            ],
-                          );
-                        });
-                  },
-                ),
-                IconButton(
-                  iconSize: 36.0,
-                  icon: Icon(Icons.delete_forever_rounded,
-                      color: Colors.redAccent),
-                  onPressed: () {
-                    Navigator.push(
-                      context,
-                      MaterialPageRoute(builder: (context) => Homepage()),
-                    );
-                  },
-                )
-              ]
-          ),
+          child: Row(mainAxisSize: MainAxisSize.max, children: <Widget>[
+            IconButton(
+              iconSize: 36.0,
+              icon: Icon(
+                Icons.color_lens_outlined,
+              ),
+              onPressed: () {
+                showDialog(
+                    context: context,
+                    builder: (BuildContext context) {
+                      return AlertDialog(
+                        title: Text('Choisis une couleur'),
+                        content: SingleChildScrollView(
+                          child: ColorPicker(
+                            pickerColor: myColor, //default color
+                            onColorChanged: (Color color) {
+                              //on color picked
+                              setState(() {
+                                myColor = color;
+                              });
+                            },
+                          ),
+                        ),
+                        actions: <Widget>[
+                          ElevatedButton(
+                            child: const Text('Valider'),
+                            onPressed: () {
+                              Navigator.of(context)
+                                  .pop(); //dismiss the color picker
+                            },
+                          ),
+                        ],
+                      );
+                    });
+              },
+            ),
+            IconButton(
+              icon: Icon(Icons.image),
+              onPressed: () {
+                showDialog(
+                    context: context,
+                    builder: (BuildContext context) {
+                      return AlertDialog(
+                        title: Text('Importer une image'),
+                        actions: <Widget>[
+                          MaterialButton(
+                              child: Icon(Icons.image),
+                              onPressed: () {
+                                pickImage(ImageSource.gallery);
+                              }),
+                          MaterialButton(
+                              child: Icon(Icons.camera_alt),
+                              onPressed: () {
+                                pickImage(ImageSource.camera);
+                              }),
+                          MaterialButton(
+                            child: Icon(Icons.close),
+                            onPressed: () {
+                              Navigator.of(context).pop();
+                            },
+                          ),
+                          SizedBox(
+                            width: 20,
+                          ),
+                        ],
+                      );
+                    });
+              },
+            ),
+            IconButton(
+              iconSize: 36.0,
+              icon: Icon(Icons.delete_forever_rounded, color: Colors.redAccent),
+              onPressed: () {
+                Navigator.push(
+                  context,
+                  MaterialPageRoute(builder: (context) => Homepage()),
+                );
+              },
+            )
+          ]),
           shape: AutomaticNotchedShape(
               RoundedRectangleBorder(), StadiumBorder(side: BorderSide())),
-        )
-    );
-  }
-
-
-  //Méthodes pour l'image picker
-  Future pickImageGallery() async {
-    try {
-      final image = await ImagePicker().pickImage(source: ImageSource.gallery);
-
-      if(image == null) return;
-
-      final imageTemp = File(image.path);
-      setState(() => this.image = imageTemp);
-    } on PlatformException catch(e){
-      print('pas pu récup l image: $e');
-    }
-  }
-
-  Future pickImageCamera() async {
-    try {
-      final image = await ImagePicker().pickImage(source: ImageSource.camera);
-
-      if(image == null) return;
-
-      final imageTemp = File(image.path);
-      setState(() => this.image = imageTemp);
-    } on PlatformException catch(e){
-      print('pas pu récup l image: $e');
-    }
+        ));
   }
 }
-
