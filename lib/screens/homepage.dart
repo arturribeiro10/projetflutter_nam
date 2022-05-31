c3052a77dddc3861f166444bcb8bae079c2f3de2
Nicolas C <nicolas.corminboeuf@he-arc.ch>
2022-06-01 01:48:33 +0200

    Création fichier searchbarmanagement.dart - contient méthode qui gère la searchbar

--- lib/screens/homepage.dart
+++ lib/screens/homepage.dart
@@ -7,6 +7,7 @@ import 'package:flutter/rendering.dart';
 import 'package:projetflutter_nam/screens/taskpage.dart';
 import 'package:projetflutter_nam/widgets.dart';
 import 'dart:async';
+import 'package:projetflutter_nam/searchbarmanagement.dart';
 
 import 'newtaskpage.dart';
 
@@ -128,82 +129,11 @@ class _HomepageState extends State<Homepage> {
   }
 }
 
-class CustomSearchDelegate extends SearchDelegate {
-  List<String> searchTerms = [
-    'Travail',
-    'Personnel',
-    'Urgent',
-    'Important',
-    'Vacances'
-  ];
 
-  @override
-  //Nettoie la requête
-  List<Widget>? buildActions(BuildContext context) {
-    return [
-      IconButton(
-        icon: const Icon(Icons.clear),
-        onPressed: () {
-          query = '';
-        },
-      )
-    ];
-  }
-
-  @override
-  //Quitter et fermer la searchbar
-  Widget buildLeading(BuildContext context) {
-    return IconButton(
-      onPressed: () {
-        close(context, null);
-      },
-      icon: const Icon(Icons.arrow_back),
-    );
-  }
-
-  @override
-  Widget buildResults(BuildContext context) {
-    List<String> matchQuery = [];
-    for (var tags in searchTerms) {
-      if (tags.toLowerCase().contains(query.toLowerCase())) {
-        matchQuery.add(tags);
-      }
-    }
-    return ListView.builder(
-      itemCount: matchQuery.length,
-      itemBuilder: (context, index) {
-        var result = matchQuery[index];
-        return ListTile(
-          title: Text(result),
-        );
-      },
-    );
-  }
-
-  @override
-  Widget buildSuggestions(BuildContext context) {
-    List<String> matchQuery = [];
-    for (var fruit in searchTerms) {
-      if (fruit.toLowerCase().contains(query.toLowerCase())) {
-        matchQuery.add(fruit);
-      }
-    }
-    return ListView.builder(
-      itemCount: matchQuery.length,
-      itemBuilder: (context, index) {
-        var result = matchQuery[index];
-        return ListTile(
-          title: Text(result),
-        );
-      },
-    );
-  }
-}
 
 /*
  * Author : Nicolas
  *  AppBar avec la barre de recherche
- *  Méthodes de "comportement" de la searchBar
  *  + changement visuelle/graphique de la page
  */
 
---
+++ lib/searchbarmanagement.dart
@@ -0,0 +1,74 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/rendering.dart';
+
+class CustomSearchDelegate extends SearchDelegate {
+  List<String> searchTerms = [
+    'Travail',
+    'Personnel',
+    'Urgent',
+    'Important',
+    'Vacances'
+  ];
+
+  @override
+  //Nettoie la requête
+  List<Widget>? buildActions(BuildContext context) {
+    return [
+      IconButton(
+        icon: const Icon(Icons.clear),
+        onPressed: () {
+          query = '';
+        },
+      )
+    ];
+  }
+
+  @override
+  //Quitter et fermer la searchbar
+  Widget buildLeading(BuildContext context) {
+    return IconButton(
+      onPressed: () {
+        close(context, null);
+      },
+      icon: const Icon(Icons.arrow_back),
+    );
+  }
+
+  @override
+  Widget buildResults(BuildContext context) {
+    List<String> matchQuery = [];
+    for (var tags in searchTerms) {
+      if (tags.toLowerCase().contains(query.toLowerCase())) {
+        matchQuery.add(tags);
+      }
+    }
+    return ListView.builder(
+      itemCount: matchQuery.length,
+      itemBuilder: (context, index) {
+        var result = matchQuery[index];
+        return ListTile(
+          title: Text(result),
+        );
+      },
+    );
+  }
+
+  @override
+  Widget buildSuggestions(BuildContext context) {
+    List<String> matchQuery = [];
+    for (var fruit in searchTerms) {
+      if (fruit.toLowerCase().contains(query.toLowerCase())) {
+        matchQuery.add(fruit);
+      }
+    }
+    return ListView.builder(
+      itemCount: matchQuery.length,
+      itemBuilder: (context, index) {
+        var result = matchQuery[index];
+        return ListTile(
+          title: Text(result),
+        );
+      },
+    );
+  }
+}
